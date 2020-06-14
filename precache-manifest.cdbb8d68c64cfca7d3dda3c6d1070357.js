c4bee048ec6d0898a67459d98f52b28d90286645
mCodex <mCodex@users.noreply.github.com>
2020-06-14 20:11:31 +0000

    deploy: b5d21eafdca2751322d2982852cb46f3554a1ea6

--- precache-manifest.cdbb8d68c64cfca7d3dda3c6d1070357.js
+++ precache-manifest.cdbb8d68c64cfca7d3dda3c6d1070357.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "30873c34c690f6c6c46704a12a4d1a3f",
+    "revision": "6f02cfafbc73c4b8f506910549756b96",
     "url": "/naval-battle/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/naval-battle/static/js/2.ea7808c3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "64e1e388d3563e3fad7c",
-    "url": "/naval-battle/static/js/main.65eef3d1.chunk.js"
+    "revision": "244dd43926c8798c8975",
+    "url": "/naval-battle/static/js/main.21b620c8.chunk.js"
   },
   {
     "revision": "984bd14426f0f5acb808",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/naval-battle/precache-manifest.482deb7e9f6830b5eea25093e22c3245.js"
+  "/naval-battle/precache-manifest.cdbb8d68c64cfca7d3dda3c6d1070357.js"
 );
 
 self.addEventListener('message', (event) => {
