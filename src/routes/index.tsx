b5d21eafdca2751322d2982852cb46f3554a1ea6
Mateus Andrade <mat.andrade@live.com>
2020-06-14 17:10:07 -0300

    fixed react-router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Home from "../pages/Home";
+import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Home} />
+    <Route path="/" component={Home} />
   </Switch>
 );
 
